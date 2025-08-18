c5cc6988c12422ba8347dcb42f64dfc69441d625
Kevin Aleman <kaleman960@gmail.com>
2025-08-18 14:35:13 -0600

    Update ee/packages/omni-core-ee/src/hooks/applyDepartmentRestrictions.ts

--- ee/packages/omni-core-ee/src/hooks/applyDepartmentRestrictions.ts
+++ ee/packages/omni-core-ee/src/hooks/applyDepartmentRestrictions.ts
@@ -7,6 +7,10 @@ import { hooksLogger } from '../utils/logger';
 
 export const applyDepartmentRestrictionsPatch = () => {
 	applyDepartmentRestrictions.patch(async (_prev, query: FilterOperators<ILivechatDepartment> = {}, userId: string) => {
+		if (!userId || !(await Authorization.hasAnyRole(userId, ['livechat-monitor']))) {
+			return originalQuery;
+		}
+
 		hooksLogger.debug('Applying department query restrictions');
 		return addQueryRestrictionsToDepartmentsModel(query, userId);
 	});
